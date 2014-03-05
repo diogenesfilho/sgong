e773607688bcd46b8ca24d4a1b72f15b3c0bebc0
diogeneslaertiussof@gmail.com <diogeneslaertiussof@gmail.com@578e65b3-2672-e3a2-ec04-a0eb9f3a0da4>
2014-03-05 21:16:05 +0000

    Criada tela de aviso a cadastro. Instancia via = new TelaAvisoCadastro();

---
+++ trunk/SG-ONG/src/view/TelaAvisoCadastro.java
@@ -0,0 +1,34 @@
+package view;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class TelaAvisoCadastro extends Stage {
+	public TelaAvisoCadastro() {
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root,550,50,Color.LINEN);
+		
+		HBox boxtexto = new HBox();
+		
+		Label texto = new Label("Cadastrado efetuado com sucesso!");
+		
+        boxtexto.getChildren().addAll(texto);
+        boxtexto.setAlignment(Pos.CENTER);
+        
+        root.setCenter(boxtexto);
+		texto.setFont(new Font(20));
+		setScene(scene);
+		setOpacity(0.9);
+		show();
+		
+	}
+}
